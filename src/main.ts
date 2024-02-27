2a91c2e0330de26a7741d57e9f173ddc8985f525
huanghewen <hhw_31@163.com>
2024-02-27 09:40:06 +0800

    vue指令

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { createApp } from 'vue';
+import { Directive, createApp, App as IApp } from 'vue';
 import { createHead } from '@unhead/vue'
 import './style.css';
 import App from '@/App.vue';
@@ -22,10 +22,31 @@ import 'vant/es/notify/style';
 import 'vant/es/image-preview/style';
 
 import 'default-passive-events'//由于 Chrome 想提高浏览器动画渲染帧数，要修改 eventListener 相关接口，出现了遮罩层无法屏蔽滚动动作的情况。
+import { copy } from './directives/copy';
+import { draggable } from './directives/draggable';
+import { debounce } from './directives/debounce';
+import { throttle } from './directives/throttle';
+import { longpress } from './directives/longpress';
 
 export const app = createApp(App);
 const head = createHead();
 const pinia = createPinia();
+
+const directivesList: { [key: string]: Directive } = {
+  copy,
+  draggable,
+  debounce,
+  throttle,
+  longpress
+};
+
+const directives = {
+  install: function (app: IApp<Element>) {
+    Object.keys(directivesList).forEach(key => {
+      app.directive(key, directivesList[key]);
+    });
+  }
+};
 
 pinia.use(
   createPersistedState({
@@ -43,5 +64,6 @@ app
   .use(head)
   .use(pinia)
   .use(router)
+  .use(directives)
   .component('MyLayout', Layout)
   .mount('#app');
